6237647b7c0d3488d2fd42f7d85c4d3ef9cd361f
krtin shet <krtin71@gmail.com>
2020-09-10 14:11:55 +0530

    modal signup implementd

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,20 @@
 const bannerSection = document.getElementsByClassName('banner')[0];
 const navbar = document.getElementsByClassName('navbar')[0];
+const signuplogin = document.getElementById('signuplogin');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const authModal = document.getElementById('authModal');
 window.addEventListener('scroll', () => {
   if (window.pageYOffset >= bannerSection.clientHeight - 50) {
     navbar.style.background = '#333';
   } else {
     navbar.style.background = 'transparent';
   }
+});
+
+closeModalBtn.addEventListener('click', function (e) {
+  authModal.style.display = 'none';
+});
+
+signuplogin.addEventListener('click', function (e) {
+  authModal.style.display = 'flex';
 });
